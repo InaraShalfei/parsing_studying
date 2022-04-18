8ba3de1fc2d5bbec83042bdf4f43eabde5331e98
InaraShalfei <inarashugaikina@gmail.com>
2022-04-18 14:33:59 +0600

    Create and register Article model

--- parsing_sites/admin.py
+++ parsing_sites/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from parsing_sites.models import Article
 
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_filter = ('date', 'title', 'sharing_count')
+
+
+admin.site.register(Article, ArticleAdmin)
--- parsing_sites/models.py
+++ parsing_sites/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=250,
+                             verbose_name='title of an article')
+    short_text = models.TextField(max_length=500,
+                                  verbose_name='first lines of an article')
+    image = models.ImageField(upload_to='photos/', verbose_name='main photo')
+    date = models.DateTimeField()
+    url = models.URLField(max_length=250, verbose_name='url of an article')
+    sharing_count = models.IntegerField(verbose_name='number of sharing')
+
+    class Meta:
+        ordering = ('date', )
+
+    def __str__(self):
+        return self.title
